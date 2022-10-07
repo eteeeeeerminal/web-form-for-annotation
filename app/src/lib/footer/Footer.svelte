b2e7e9bb1051ba1d0a8ba0b72f6f07815503d30d
えてみなる <47494433+eteeeeeerminal@users.noreply.github.com>
2022-10-07 18:26:28 +0900

    feat: footer 下部に固定

--- app/src/lib/footer/Footer.svelte
+++ app/src/lib/footer/Footer.svelte
@@ -2,3 +2,11 @@
 	<p>Web Form For Annotation © 2022 eteeeeeerminal</p>
 	<div>GitHub link</div>
 </footer>
+
+<style lang="scss">
+	footer {
+		border-top: ridge thin gray;
+		top: 100vh;
+		position: sticky;
+	}
+</style>
