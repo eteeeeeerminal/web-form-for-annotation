7e1c33ee03fc237caf98407002cdebdbbb233532
えてみなる <47494433+eteeeeeerminal@users.noreply.github.com>
2022-10-09 20:20:46 +0900

    chore: install svelte material ui

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -3,6 +3,17 @@
 	import Footer from '$lib/footer/Footer.svelte';
 </script>
 
+<svelte:head>
+	<!-- Roboto -->
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700"
+	/>
+	<!-- Roboto Mono -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Mono" />
+	<link rel="stylesheet" href="/smui.css" />
+</svelte:head>
+
 <Header />
 
 <main>
