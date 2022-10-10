81a5424659235a2dd612567334011d8e7b7b1757
えてみなる <47494433+eteeeeeerminal@users.noreply.github.com>
2022-10-10 14:32:38 +0900

    feat: loginなしでデータセットページに行くとリダイレクトするように

---
+++ app/src/routes/dataset/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { currentUser } from '$lib/api/auth';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		if ($currentUser == null) {
+			goto('/');
+		}
+	});
+</script>
+
+{#if $currentUser == null}
+	<h2>トップページへリダイレクトします…</h2>
+{:else}
+	<slot />
+{/if}
