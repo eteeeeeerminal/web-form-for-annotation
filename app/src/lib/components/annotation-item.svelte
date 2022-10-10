f05bc8142fcbadec6cb8ccf899faf5cdb8139084
えてみなる <47494433+eteeeeeerminal@users.noreply.github.com>
2022-10-10 16:21:53 +0900

    feat: youtube の動画表示だいたい完成

---
+++ app/src/lib/components/annotation-item.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Card from '@smui/card';
+</script>
+
+<Card style="margin: 1rem 0.2rem; overflow: hidden">
+	<div class="annotation-item">
+		<slot />
+	</div>
+</Card>
+
+<style lang="scss">
+	.annotation-item {
+		max-width: 640px;
+		width: 100vw;
+	}
+</style>
--- app/src/lib/components/dataset/YouTubeVideo.svelte
+++ app/src/lib/components/dataset/YouTubeVideo.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import AnnotationItem from '../annotation-item.svelte';
+
 	export let videoId: string;
 	export let title: string;
 	export let uploaderName: string;
@@ -12,20 +14,16 @@
 	$: descriptionOmitted = true || videoId;
 </script>
 
-<iframe
-	src="https://www.youtube.com/embed/{videoId}"
-	title="YouTube video player"
-	frameborder="0"
-	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-	allowfullscreen
-/>
-
-<div>
-	<h3>
-		{title}
-	</h3>
-	<div>
-		{uploaderName}
+<AnnotationItem>
+	<div class="video-wrapper">
+		<iframe
+			src="https://www.youtube.com/embed/{videoId}"
+			class="video"
+			title="YouTube video player"
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+			allowfullscreen
+		/>
 	</div>
 
 	<div>
@@ -44,17 +42,54 @@
 			{descriptionOmitted ? 'もっと見る' : '一部を表示'}
 		</button>
 	</div>
-	<button on:click={switchExpand} style={'background: none; border: none; cursor: pointer;'}>
-		{descriptionOmitted ? 'もっと見る' : '一部を表示'}
-	</button>
-</div>
+</AnnotationItem>
 
-<style>
+<style lang="scss">
+	.video {
+		position: absolute;
+		top: 0;
+		right: 0;
+		width: 100%;
+		height: 100%;
+	}
+	.video-wrapper {
+		position: relative;
+		width: 100%;
+		padding-top: 56.25%;
+	}
+
+	.title {
+		font-weight: bold;
+		font-size: large;
+		margin: 0.5rem 0.4rem 0.2rem 0.4rem;
+	}
+
+	.description {
+		margin: 0.4rem 1rem 0rem 1rem;
+	}
+
+	.name {
+		display: inline-block;
+		margin: 0px 0px 1rem;
+		color: $text-color;
+		text-decoration: none;
+	}
+
 	.omitted {
 		overflow: hidden;
 		height: 4.5rem;
 		text-overflow: clip;
 	}
 	.expanded {
+		overflow-y: auto;
+		max-height: 13.5rem;
+	}
+
+	.expandButton {
+		color: dimgray;
+		background: none;
+		border: none;
+		cursor: pointer;
+		margin: 0.2rem 1rem 0.2rem 1rem;
 	}
 </style>
