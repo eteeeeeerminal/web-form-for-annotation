9bfc1ab3fcb212f84e64b3fca83bb5a105210a62
えてみなる <47494433+eteeeeeerminal@users.noreply.github.com>
2022-10-12 20:56:47 +0900

    feat: ページ送り用のボタン作成 回答ボタン調整

--- app/src/lib/form/PaginationButton.svelte
+++ app/src/lib/form/PaginationButton.svelte
@@ -1,21 +1,19 @@
 <script lang="ts">
 	import Button from '@smui/button';
+	export let name: string;
 </script>
 
-<div class="submit">
-	<Button style="width: 100%; height: 100%" type="submit" color="secondary" variant="unelevated">
-		<div class="button-word">送信</div>
+<div class="pagination">
+	<Button style="width: 100%; height: 100%" color="primary" variant="unelevated">
+		<div>{name}</div>
 	</Button>
 </div>
 
 <style lang="scss">
-	.submit {
+	.pagination {
 		display: inline-block;
 		width: 4.5rem;
 		height: 2.5rem;
 		margin: 0.2rem 0.6rem 1rem 0;
-	}
-	.button-word {
-		font-size: larger;
 	}
 </style>
