bceafbd065a54afce92aba735ffb2180bd8b599b
えてみなる <47494433+eteeeeeerminal@users.noreply.github.com>
2022-10-12 20:44:13 +0900

    feat: create SubmitButton

---
+++ app/src/lib/form/SubmitButton.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Button from '@smui/button';
+</script>
+
+<div class="submit">
+	<Button style="padding: 1.2rem 1.4rem" type="submit" color="secondary" variant="unelevated">
+		<div class="button-word">回答</div>
+	</Button>
+</div>
+
+<style lang="scss">
+	.submit {
+		width: fit-content;
+		margin: 0.6rem 0 1.5rem;
+	}
+	.button-word {
+		font-size: larger;
+	}
+</style>
