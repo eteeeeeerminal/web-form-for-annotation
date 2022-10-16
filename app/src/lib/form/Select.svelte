7ad76e5c23cbc391ee03f25162968b53b98494da
えてみなる <47494433+eteeeeeerminal@users.noreply.github.com>
2022-10-16 20:27:29 +0900

    feat: Select 要素実装

---
+++ app/src/lib/form/Select.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { createField } from 'felte';
+	import Select, { Option } from '@smui/select';
+
+	export let prop: SelectProp;
+
+	let selected = prop.initStat ? prop.initStat : '';
+
+	const { field, onInput } = createField(prop.name);
+
+	$: onInput(selected);
+</script>
+
+<div use:field>
+	<Select
+		bind:value={selected}
+		label="選んでください"
+		variant="outlined"
+		style="margin: 0.6rem 1rem 0.6rem;"
+	>
+		<Option value="" />
+		{#each prop.values as value}
+			<Option {value}>{value}</Option>
+		{/each}
+	</Select>
+</div>
