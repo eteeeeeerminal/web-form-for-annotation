0e73451aef72437168749b71b23cb41cfb30a033
えてみなる <47494433+eteeeeeerminal@users.noreply.github.com>
2022-10-25 13:58:16 +0900

    feat: 回答状況表示ざっくり

--- app/src/lib/vtuber-onomatopoeia/components/admin-page/AdminPage.svelte
+++ app/src/lib/vtuber-onomatopoeia/components/admin-page/AdminPage.svelte
@@ -1,21 +1,48 @@
 <script lang="ts">
-	// 回答状況一覧表示
-	// 締め切る / 募集開始 ボタン
-	// データのダウンロード実装
-	// ng データ一覧取得して表示ボタン
-
 	import { getUserData } from '$lib/dataset/user-data';
 	import { updateDataset } from '$lib/api/database';
 	import AnnotationFormItem from '$lib/form/AnnotationFormItem.svelte';
 	import PrimaryButton from '$lib/button/PrimaryButton.svelte';
 	import { datasetId } from '$lib/vtuber-onomatopoeia/dataset/database';
 
-	const { annotationLog, annotationCounts } = getUserData(datasetId);
+	const { annotationCounts } = getUserData(datasetId);
+
+	const shapeCounts = (counts: AnnotationCounts | null) => {
+		if (counts == null) return null;
+		const ret = new Map();
+		counts.forEach((value) => {
+			if (ret.has(value)) {
+				ret.set(value, ret.get(value) + 1);
+			} else {
+				ret.set(value, 1);
+			}
+		});
+		return ret;
+	};
 
 	$: readyDataset = Boolean($annotationCounts);
+	$: counts = shapeCounts($annotationCounts);
 </script>
 
 <AnnotationFormItem>
+	<h4>回答状況一覧</h4>
+	<p>
+		データ総数: {$annotationCounts?.size}
+		{#if counts}
+			<br />
+			アノテーションされた回数: データ数 <br />
+			{#each [...counts] as count}
+				{count[0]}回: {count[1]}<br />
+			{/each}
+		{/if}
+	</p>
+	回答状況読み込みボタン
+</AnnotationFormItem>
+
+<AnnotationFormItem>ngデータ一覧取得ボタン/ ngデータ一覧</AnnotationFormItem>
+
+<AnnotationFormItem>
+	締め切り/募集開始ボタン
 	<PrimaryButton
 		label={readyDataset ? 'データセットを更新' : 'データセットを準備'}
 		on:click={async () => {
@@ -23,4 +50,5 @@
 			location.reload();
 		}}
 	/>
+	データのダウンロードボタン
 </AnnotationFormItem>
